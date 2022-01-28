ad81a670f7d9f651b3c6162a7560ec96f20a82fe
nichujie <ncj19991213@126.com>
2022-01-28 12:09:28 +0100

    feat: add LIC 1 (#5)

--- src/main/java/com/group12/LaunchInterceptorConditionCollection.java
+++ src/main/java/com/group12/LaunchInterceptorConditionCollection.java
@@ -10,7 +10,7 @@ public class LaunchInterceptorConditionCollection {
      * LIC #0 checks if there exists at least one set of two consecutive data points that are a distance greater than
      * the given <b>length1</b>.
      *
-     * @param points list of radar echos ({@link Point})
+     * @param points  list of radar echos ({@link Point})
      * @param length1 has to be greater than 0 (zero)
      * @return true if two consecutive points are greater than the given length1, false otherwise.
      * @throws IllegalArgumentException is thrown if <b>length1</b> is less than 0 (zero)
@@ -37,17 +37,50 @@ public class LaunchInterceptorConditionCollection {
     }
 
     /**
+     * LIC #1 checks if there exists at least one set of three consecutive data points that cannot all be contained
+     * within or on a circle of the given <b>radius</b> (RADIUS1).
+     *
+     * @param points  list of radar echos ({@link Point})
+     * @param radius1 has to be greater than 0 (zero)
+     * @return true if at least one set of three consecutive data points forms a circle with radius greater than
+     * given <b>radius</b>, false otherwise.
+     * @throws IllegalArgumentException is thrown if <b>radius</b> is less than 0 (zero)
+     */
+    public boolean LIC1(List<Point> points, double radius1) throws IllegalArgumentException {
+        if (points == null) {
+            throw new IllegalArgumentException("Points list cannot be null");
+        }
+        if (points.size() < 3) {
+            throw new IllegalArgumentException("There has to be at least three points, size of given points="
+                    + points.size());
+        }
+        if (radius1 < 0) {
+            throw new IllegalArgumentException("Radius cannot be less than zero");
+        }
+        for (int i = 0; i < points.size() - 2; i++) {
+            Point point1 = points.get(i);
+            Point point2 = points.get(i + 1);
+            Point point3 = points.get(i + 2);
+            if (radiusOfSmallestCircle(point1, point2, point3) > radius1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * LIC #7 checks if at least one set of two data points separated by exactly <b>kPts</b> consecutive intervening
      * points that are a distance greater than <b>length1</b> apart. The condition is not met when <b>points</b> has
      * less than three elements.
-     * @param points points list of radar echos ({@link Point})
-     * @param kPts number of consecutive intervening radar echos
+     *
+     * @param points  points list of radar echos ({@link Point})
+     * @param kPts    number of consecutive intervening radar echos
      * @param length1 has to be greater than 0 (zero)
      * @return true if two points <b>kPts</b> apart has a distance greater than <b>length1</b> or number of radar echos
      * is less than 3, false otherwise.
      * @throws IllegalArgumentException is thrown if <b>points</b> is null, if
-     * <b>kPts</b> is less than 1 or greater than two less than size of <b>points</b>, or if <b>length1</b> is less
-     * than 0 (zero)
+     *                                  <b>kPts</b> is less than 1 or greater than two less than size of <b>points</b>, or if <b>length1</b> is less
+     *                                  than 0 (zero)
      */
     public boolean LIC7(List<Point> points, int kPts, double length1) throws IllegalArgumentException {
         if (points == null) {
@@ -78,5 +111,42 @@ public class LaunchInterceptorConditionCollection {
         double x2 = point2.getX();
         double y2 = point2.getY();
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    private int doubleCompare(double a, double b) {
+        if (Math.abs(a - b) < 0.000001) return 0;
+        if (a < b) return -1;
+        return 1;
+    }
+
+    private double radiusOfSmallestCircle(Point point1, Point point2, Point point3) {
+        double x1 = point1.getX();
+        double y1 = point1.getY();
+        double x2 = point2.getX();
+        double y2 = point2.getY();
+        double x3 = point3.getX();
+        double y3 = point3.getY();
+
+        // Cramer's Rule
+        double a = 2 * (x1 - x2);
+        double b = 2 * (y1 - y2);
+        double c = x1 * x1 + y1 * y1 - x2 * x2 - y2 * y2;
+        double d = 2 * (x1 - x3);
+        double e = 2 * (y1 - y3);
+        double f = x1 * x1 + y1 * y1 - x3 * x3 - y3 * y3;
+        double g = a * e - b * d;
+
+        // If g=0, the three points are linear. The longest distance among three points is the diameter.
+        if (doubleCompare(g, 0.0) == 0) {
+            double d1 = distance(point1, point2);
+            double d2 = distance(point2, point3);
+            double d3 = distance(point1, point3);
+            return Math.max(Math.max(d1, d2), d3) / 2;
+        }
+
+        // Otherwise, calculate the center of circle. The distance should be equal to 3 points.
+        double circleX = (c * e - f * b) / g;
+        double circleY = (a * f - d * c) / g;
+        return distance(point1, new Point(circleX, circleY));
     }
 }
