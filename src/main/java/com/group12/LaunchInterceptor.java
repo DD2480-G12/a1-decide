66211b3ba5ac72abd411d3fcaa295b5a46e743fd
Mustafa Dinler <dinlermstf@gmail.com>
2022-01-29 11:58:39 +0100

    refactor: add initial main class for DECIDE()
    App.java and AppTest.java were two files generated by the maven
    generate command. In order to have something more suitable for the
    program, these two files is now replaced with the main class that has
    the DECIDE() function, LaunchInterceptor.java
    
    DECIDE() itself is missing implementation and is empty. This has to be
    implemented later.

---
+++ src/main/java/com/group12/LaunchInterceptor.java
@@ -0,0 +1,13 @@
+package com.group12;
+
+public class LaunchInterceptor {
+
+    public void decide() {
+
+    }
+
+    public static void main(String[] args) {
+        LaunchInterceptor launchInterceptor = new LaunchInterceptor();
+        launchInterceptor.decide();
+    }
+}
