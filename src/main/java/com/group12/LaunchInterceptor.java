acc3580534da753ae1ba5477e66d99478ee337ad
Gustav Ekner <gustav.ekner@gmail.com>
2022-02-01 17:53:15 +0100

    feat: input and constants (#19)

--- src/main/java/com/group12/LaunchInterceptor.java
+++ src/main/java/com/group12/LaunchInterceptor.java
@@ -1,6 +1,22 @@
 package com.group12;
 
+import com.group12.model.Connector;
+import com.group12.model.Point;
+
+import java.util.List;
+
 public class LaunchInterceptor {
+
+    // LIC parameters:
+    Parameters parameters;
+
+    // Input:
+    public List<Point> points;
+    public Connector[][] lcm;
+    public boolean[] puv;
+
+    // Output:
+    boolean launch;
 
     public void decide() {
 
