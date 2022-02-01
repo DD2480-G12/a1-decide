938043336ab6e342364b94381aed6e0d7ee5dbe6
Gustav Ekner <gustav.ekner@gmail.com>
2022-02-01 17:53:15 +0100

    feat: input and constants (#19)

---
+++ src/main/java/com/group12/Parameters.java
@@ -0,0 +1,27 @@
+package com.group12;
+
+/**
+ * Parameters for the Launch Interceptor Conditions.
+ */
+public class Parameters {
+
+    public double length1 = 2;
+    public double radius1 = 3;
+    public double epsilon = 1.4;
+    public double area1 = 2;
+    public int qPts = 3;
+    public int quads = 2;
+    public double dist = 4;
+    public int nPts = 4;
+    public int kPts = 3;
+    public int aPts = 1;
+    public int bPts = 2;
+    public int cPts = 1;
+    public int dPts = 2;
+    public int ePts = 1;
+    public int fPts = 2;
+    public int gPts = 3;
+    public double length2 = 3;
+    public double radius2 = 2;
+    public double area2 = 5;
+}
