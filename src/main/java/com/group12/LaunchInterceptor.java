10b28b18f792942fddc36213af2c7026491c7bca
Mustafa Dinler <dinlermstf@gmail.com>
2022-02-02 08:53:10 +0100

    feat: implement decide function (#3)

--- src/main/java/com/group12/LaunchInterceptor.java
+++ src/main/java/com/group12/LaunchInterceptor.java
@@ -3,9 +3,14 @@ package com.group12;
 import com.group12.model.Connector;
 import com.group12.model.Point;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class LaunchInterceptor {
+
+    private final LaunchInterceptorCore launchInterceptorCore;
 
     // LIC parameters:
     Parameters parameters;
@@ -18,7 +23,32 @@ public class LaunchInterceptor {
     // Output:
     boolean launch;
 
-    public void decide() {
+    public LaunchInterceptor(Parameters parameters, List<Point> points, Connector[][] lcm, boolean[] puv) {
+        this.launchInterceptorCore = new LaunchInterceptorCore();
+        this.parameters = parameters;
+        this.points = points;
+        this.lcm = lcm;
+        this.puv = puv;
+    }
 
+    public void decide() {
+        try {
+            boolean[] cmv = launchInterceptorCore.calculateCMV(parameters, points);
+            boolean[][] pum = launchInterceptorCore.calculatePUM(cmv, lcm);
+            boolean[] fuv = launchInterceptorCore.calculateFUV(pum, puv);
+            launch = isAllTrue(fuv);
+            System.out.println(launch ? "YES" : "NO");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid input. " + e.getMessage());
+        }
+    }
+
+    private boolean isAllTrue(boolean[] booleans) {
+        for (boolean b : booleans) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
     }
 }
